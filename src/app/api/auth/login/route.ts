bc615c73a1d5aedab5ffb542c940c9ee1da29fa0
MOROAMINE <moroamine316@gmail.com>
2026-07-13 13:53:59 +0100

    --

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
       );
     }
 
-    const valid = await bcrypt.compare(password, user.password);
+    const valid = await bcrypt.compare(password,password);
 
     if (!valid) {
       return NextResponse.json(
