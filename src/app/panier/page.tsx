2306831a6ae920930972cc6ae1366cfd6d834ab7
MOROAMINE <moroamine316@gmail.com>
2026-07-14 16:36:27 +0100

    --

--- src/app/panier/page.tsx
+++ src/app/panier/page.tsx
@@ -131,7 +131,7 @@ export default function PanierPage() {
               </div>
 
               <Link
-                href="/client/login"
+                href="/client/inscription"
                 className="block text-center bg-green-600 hover:bg-green-700 text-white py-3 rounded-lg mt-6"
               >
                 Passer la commande
